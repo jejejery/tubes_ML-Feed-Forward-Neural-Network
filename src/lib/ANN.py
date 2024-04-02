d629b36c744011185b9ba8dd6afc6e74751534fa
jejejery <13521131@std.stei.itb.ac.id>
2024-04-02 14:32:10 +0700

    hmmmm

--- src/lib/ANN.py
+++ src/lib/ANN.py
@@ -11,6 +11,14 @@ class ANN:
         self.output_size = output_size
 
     def add(self, layer):
+        #check if the input shape of the new layer is the same as the output shape of the last layer (if filled)
+        if(len(self.layers) > 0 ):
+            if(layer.input_shape != self.layers[-1].output_shape):
+                raise ValueError(f"Invalid input shape. Expected: {self.layers[-1].output_shape}")
+        else:
+            if(layer.input_shape != self.input_size):
+                raise ValueError(f"Invalid input shape. Expected: {self.input_size}")
+    
         #if the last layer is output layer, then it's not valid to add another hidden layer
         if(len(self.layers) > 0 and self.layers[-1].layer_type == "output"):
                 raise ValueError("OutputLayer already exist!")
