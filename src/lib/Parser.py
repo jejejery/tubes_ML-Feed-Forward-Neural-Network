e8a32ad0e466e09b21c9055ee1e0252d1b0a6b14
jejejery <13521131@std.stei.itb.ac.id>
2024-04-04 21:38:26 +0700

    finalize

--- src/lib/Parser.py
+++ src/lib/Parser.py
@@ -10,7 +10,7 @@ class Parser:
             data = json.load(json_file)
         case = data["case"]
         self.input = np.array(case["input"])
-        self.weights = np.array(case["weights"])
+        self.weights = list(case["weights"])
 
         model = case["model"]
         self.input_size = model["input_size"]
@@ -20,9 +20,6 @@ class Parser:
         self.expected_output = np.array(expect["output"])
         self.max_sse = expect["max_sse"]
 
-        print(type(self.input_size))
-        print(self.input[0])
-    
     def getInputSize(self):
         return self.input_size
     
@@ -33,18 +30,29 @@ class Parser:
         layers_size = len(self.layers)
         for i in range(layers_size):
             if i == 0:
-                layer = HiddenLayer(name=f"hidden{i+1}", input_shape=self.input_size, output_shape=self.layers[i]["number_of_neurons"], weights=self.weights[i], activation_function=self.layers[i]["activation_function"])
+                layer = HiddenLayer(name=f"hidden{i+1}", input_shape=self.input_size, output_shape=self.layers[i]["number_of_neurons"], weights=np.array(self.weights[i]), activation_function=self.layers[i]["activation_function"])
                 model.add(layer)
             elif i!=(layers_size-1):
-                layer = HiddenLayer(name=f"hidden{i+1}", input_shape=self.layers[i-1]["number_of_neurons"], output_shape=self.layers[i]["number_of_neurons"], weights=self.weights[i], activation_function=self.layers[i]["activation_function"])
+                layer = HiddenLayer(name=f"hidden{i+1}", input_shape=self.layers[i-1]["number_of_neurons"], output_shape=self.layers[i]["number_of_neurons"], weights=np.array(self.weights[i]), activation_function=self.layers[i]["activation_function"])
                 model.add(layer)
             else:
-                layer = OutputLayer(name="output1",input_shape=3, output_shape=2, weights=self.weights[i], activation_function=self.layers[i]["activation_function"])
+                layer = OutputLayer(name="output1",input_shape=self.layers[i-1]["number_of_neurons"], output_shape=self.layers[i]["number_of_neurons"], weights=np.array(self.weights[i]), activation_function=self.layers[i]["activation_function"])
                 model.add(layer)
 
-    def getExpectedOutpu(self):
+    def getExpectedOutput(self):
         return np.array(self.expected_output)
     
     def getMaxSse(self):
         return self.max_sse
+    
+    def getSse(self,prediction):
+        res = self.getExpectedOutput() - prediction
+        res = res**2
+        #flatten the array
+        res = res.flatten()
+        return np.sum(res)
+
+    def isCorrect(self, prediction):
+        return self.getSse(prediction) <= self.getMaxSse()
+        
     
