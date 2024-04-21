fix softmax output

src/lib/ANN.py
@@ -5,7 +5,7 @@ from lib.HiddenLayer import HiddenLayer
 from lib.OutputLayer import OutputLayer
 
 class ANN:
-    def __init__(self, input_size, output_size, learning_rate=1e-2, max_epoch=1000, tolerance=1e-5):
+    def __init__(self, input_size, output_size, learning_rate=1e-1, max_epoch=1000, tolerance=1e-5):
         self.input_size = input_size
         self.layers = []
         self.output_size = output_size
@@ -75,6 +75,14 @@ class ANN:
     def reset_delta_weights(self):
         for layer in self.layers:
             layer.delta_weights = np.zeros(layer.weights.shape)
+
+    def set_expected_output(self, Y):
+        if self.layers[-1].layer_type != "output":
+            raise ValueError("The last layer must be output layer")
+        self.layers[-1].set_expected_output(Y)
+
+    def get_current_loss(self):
+        return self.layers[-1].loss
         
           
             

src/lib/Model.py
@@ -7,7 +7,12 @@ class Model:
     def __init__(self, name, ann : ANN):
         self.name = name
         self.ann = ann
-        self.test_input = None
+        self.train_input = None
+        self.train_output = None
+        self.valid_input = None
+        self.valid_output = None
+        self.loses = []
+
 
     """
     build the model like example testcase in .json file
@@ -44,3 +49,51 @@ class Model:
     
     def test_backward(self):
         self.ann.backward_propagation()
+
+    """
+    Please change the validation part to use the validation set
+    """
+    def fit(self, X_train : np.array, Y_train : np.array, epochs : int, learning_rate : float, batch_size : int = 1, X_valid : np.array = None, Y_valid : np.array = None, error_threshold : float = 1e-5):
+        # check if the last layer is output layer
+        if self.ann.layers[-1].layer_type != "output":
+            raise ValueError("The last layer must be output layer")
+        if(len(X_train.shape) == 1):
+            X_train = X_train.reshape(X_train.shape[0], 1)
+        #if input shape invalid
+        if(X_train.shape[1] != self.ann.input_size):
+            raise ValueError(f"Invalid input size. Expected: {self.ann.input_size}, got: {X_train.shape[1]}")
+        
+        # configure ANN
+        self.ann.learning_rate = learning_rate
+
+
+        #train
+        self.train_input = X_train
+        self.train_output = Y_train
+
+        #valid
+        #self.valid_input = X_valid
+        #self.valid_output = Y_valid
+        for i in range(epochs):
+            #looping for each batch
+            for j in range(0, X_train.shape[0], batch_size):
+                #get the batch
+                X_batch = X_train[j:j+batch_size]
+                Y_batch = Y_train[j:j+batch_size]
+                self.ann.layers[-1].expected_output = Y_batch
+                #forward propagation
+                self.ann.forward_propagation(X_batch)
+                #backward propagation
+                self.ann.backward_propagation()
+            
+            #break if the error is below the threshold
+            if self.ann.get_current_loss() < error_threshold:
+                break
+
+
+            
+            print(f"Epoch {i+1} completed")
+            print(f"Loss: {self.ann.get_current_loss()}")
+            self.loses.append(self.ann.get_current_loss())
+            #print(f"Accuracy: {self.ann.accuracy(Y_train)}")
+        

src/lib/OutputLayer.py
@@ -68,18 +68,11 @@ class OutputLayer(Layer):
     def softmax_gradient(self):
         #find argmax of each row for the expected output
         pk = np.argmax(self.expected_output, axis=1)
-        #find argmax of each row for the current output
-        pk_hat = np.argmax(self.current_output, axis=1)
-
-        is_same = (pk == pk_hat) #np array
-
-        # clone the current output
+        
         current_output_clone = np.array(self.current_output)
-        for i in range(len(is_same)):
-            if is_same[i]:
-                current_output_clone[i][pk_hat[i]] = -(1-self.current_output[i][pk_hat[i]])
-            else:
-                current_output_clone[i] = -(1-self.current_output[i])
-                current_output_clone[i][pk_hat[i]] = self.current_output[i][pk_hat[i]]
+        # sort the current output in descending order
+        -np.sort(-current_output_clone, axis=1)
+        for i in range(len(current_output_clone)):
+            current_output_clone[i][pk[i]] = -(1-current_output_clone[i][pk[i]])
 
         return current_output_clone